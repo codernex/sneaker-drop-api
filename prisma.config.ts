6931f4ae68a6568dbcb0204bc59eefe4d27bb1e1
Borhan Uddin <borhan.dev@gmail.com>
2026-06-19 13:08:46 +0600

    feat: update prisma to use the latest version & fixed adapter issues

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,12 @@
-import "dotenv/config";
+import { env } from "./src/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+  },
+  datasource: {
+    url: env.DATABASE_URL,
   },
 });
